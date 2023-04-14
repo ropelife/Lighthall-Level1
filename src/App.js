393538df905d0be95d6985fbe857fddee8c580b3
ropelife <45571370+ropelife@users.noreply.github.com>
2023-04-14 14:27:27 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [clickCount, setClickCount] = useState(parseInt(localStorage.getItem('clickCount') || '0'));
+  const [location, setLocation] = useState(JSON.parse(localStorage.getItem('location') || 'null'));
+  const [cityCounts, setCityCounts] = useState(JSON.parse(localStorage.getItem('cityCounts') || '[]'));
+
+  const handleClick = () => {
+    const newClickCount = clickCount + 1;
+    setClickCount(newClickCount);
+
+    // get city name from latitude and longitude
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${location.latitude}&longitude=${location.longitude}&localityLanguage=en`)
+      .then(response => response.json())
+      .then(data => {
+        const cityName = data.city;
+        const countryName = data.countryName;
+        const cityIndex = cityCounts.findIndex(cityCount => cityCount.cityName === cityName && cityCount.countryName === countryName);
+
+        if (cityIndex === -1) {
+          setCityCounts([...cityCounts, { cityName, countryName, count: 1 }]);
+        } else {
+          const newCityCounts = [...cityCounts];
+          newCityCounts[cityIndex].count += 1;
+          setCityCounts(newCityCounts);
+        }
+      });
+  }
+
+  useEffect(() => {
+    if (!location) {
+      // get user's current location
+      navigator.geolocation.getCurrentPosition(position => {
+        const newLocation = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+        setLocation(newLocation);
+        localStorage.setItem('location', JSON.stringify(newLocation));
+      });
+    }
+  }, [location]);
+
+  useEffect(() => {
+    localStorage.setItem('clickCount', clickCount);
+  }, [clickCount]);
+
+  useEffect(() => {
+    localStorage.setItem('cityCounts', JSON.stringify(cityCounts));
+  }, [cityCounts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <button onClick={handleClick} className="myButton btn btn-primary">Click Me</button>
+      <p>Click count: {clickCount}</p>
+      <h4>Distribution of clicks by Geographic Location:</h4>
+
+      <table className='table-container'>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Country</th>
+            <th>Click Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cityCounts.map(cityCount => (
+            <tr key={`${cityCount.cityName}-${cityCount.countryName}`}>
+              <td>{cityCount.cityName}</td>
+              <td>{cityCount.countryName}</td>
+              <td>{cityCount.count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
